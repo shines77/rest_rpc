7c31d3e22056ff511c39d0893ce5c50b91e4db3e
shines77 <gz_shines@msn.com>
2016-06-07 14:28:07 +0800

    perf_test的bat文件的一处小修改;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- perf_test/perf_test_except.bat
+++ perf_test/perf_test_except.bat
@@ -1,3 +1,3 @@
 
-perf_test.exe --server-ip 127.0.0.1 --server-port 9000 --ip 127.0.0.1 --port 9010 --mode=rpc-call3 --test=qps3 --rpc-topic fun2
+perf_test.exe --host 127.0.0.1 --port 9000 --mode=rpc-call3 --test=qps3 --rpc-topic fun2
 pause
